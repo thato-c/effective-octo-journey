dc555e9ef043e326c44dd64964bbb5ea0fc188aa
Thato <t.chitja369@gmail.com>
2023-09-19 22:45:22 +0200

    Initial Project Creation

---
+++ Octo_Journey/Octo_Journey/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Octo_Journey.Models;
+using System.Diagnostics;
+
+namespace Octo_Journey.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Octo_Journey/Octo_Journey/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Octo_Journey.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
