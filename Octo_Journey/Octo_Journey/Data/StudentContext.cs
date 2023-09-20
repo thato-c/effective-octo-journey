7ae581b349f34ec6111e3a379b15fb3d82eced7a
Thato <t.chitja369@gmail.com>
2023-09-20 08:07:12 +0200

    Scaffold student pages

---
+++ Octo_Journey/Octo_Journey/Data/StudentContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using ContosoUniversity.Models;
+
+namespace Octo_Journey.Data
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; } = default!;
+    }
+}
--- Octo_Journey/Octo_Journey/Program.cs
+++ Octo_Journey/Octo_Journey/Program.cs
@@ -1,4 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Octo_Journey.Data;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<StudentContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("StudentContext") ?? throw new InvalidOperationException("Connection string 'StudentContext' not found.")));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
